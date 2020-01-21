736cf4502fd4ea3832529317a8695963ebc7fefe
Ravisankar <rasa97uchiha@gmail.com>
2020-01-21 22:14:08 +0100

    Added Header for vector util

--- src/main/java/tudelft/cgv/volvis/RaycastRenderer.java
+++ src/main/java/tudelft/cgv/volvis/RaycastRenderer.java
@@ -6,6 +6,7 @@ import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.util.texture.Texture;
 import com.jogamp.opengl.util.texture.awt.AWTTextureIO;
+import com.jogamp.opengl.math.VectorUtil;
 import tudelft.cgv.gui.RaycastRendererPanel;
 import tudelft.cgv.gui.TransferFunction2DEditor;
 import tudelft.cgv.gui.TransferFunctionEditor;
